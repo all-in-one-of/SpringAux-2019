beeaaf4a9de62968655c5676cfeee2846c6d59de
Colin <colin.hite@gmail.com>
2019-04-08 15:43:04 -0600

    Made great progress on the assign materials script

---
+++ DGM-3641_GameLevel/ProdOfEH_Hite/ProdOfEH_Hite/Assets/Editor/CreateMaterialWindow.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+using System.IO;
+using JetBrains.Annotations;
+using NUnit.Framework.Internal;
+using UnityEditor.LinuxStandalone;
+using UnityEngine.Windows;
+using File = System.IO.File;
+
+public class CreateMaterialWindow : EditorWindow
+{
+    public string colorSuffix = "";
+    public string metallicSuffix = "";
+    public string heightSuffix = "";
+    public string normalSuffix = "";
+    public string occlusionSuffix = "";
+
+    public string[] newSuffix;
+    
+    [MenuItem("Window/CreateMaterialsTool #&w")]
+    public static void ShowWindow ()
+    {
+        EditorWindow.GetWindow(typeof(CreateMaterialWindow));
+        Debug.Log("You are creating the window");
+    }
+    
+    
+    void OnGUI ()
+    {
+        newSuffix = new string[] {colorSuffix, metallicSuffix, heightSuffix, normalSuffix, occlusionSuffix};
+        
+        colorSuffix = GUI.TextField(new Rect(10, 10, 200, 20), colorSuffix, 25);
+        metallicSuffix = GUI.TextField(new Rect(10, 30, 200, 20), metallicSuffix, 25);
+        heightSuffix = GUI.TextField(new Rect(10, 50, 200, 20), heightSuffix, 25);
+        normalSuffix = GUI.TextField(new Rect(10, 70, 200, 20), normalSuffix, 25);
+        occlusionSuffix = GUI.TextField(new Rect(10, 90, 200, 20), occlusionSuffix, 25);
+        
+        
+        if (GUI.Button(new Rect(10, 130, 200, 30), "SavePreferences"))
+        {
+            OperateSaveFile(newSuffix);
+        }
+    }
+
+    public void OperateSaveFile(string[] newSuffix)
+    {
+        if (File.Exists("Assets/ColinToolsPreferences.txt"))
+        {
+            Debug.Log("Colin Tools have been updated.");
+            StreamWriter test = File.CreateText("Assets/ColinToolsPreferences.txt");
+            for (int i = 0; i < newSuffix.Length; i++)
+            {
+                test.WriteLine(newSuffix[i]);
+            }
+
+            test.Close();
+        }
+        else
+        {
+            StreamWriter test = File.CreateText("Assets/ColinToolsPreferences.txt");
+            test.Write("Null");
+            test.Close();
+        }
+    }
+}
---
+++ DGM-3641_GameLevel/ProdOfEH_Hite/ProdOfEH_Hite/Assets/Editor/EditorProtoEnv1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Content;
+using UnityEditor.VersionControl;
+using UnityEngine.UI;
+using UnityEngine.WSA;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+
+public class EditorProtoEnv : EditorWindow {
+
+    private static string selectedObjectName;
+    private static Material passMaterial;
+
+    public CreateMaterialWindow createMaterialWindow;
+
+    public StreamReader test = new StreamReader("Assets/ColinToolsPreferences.txt");
+    
+    //When I press a button I want a texture to be created with the name of the folder I have selected
+    [MenuItem("Tools/TestEditor &w")]
+    public static void TestEditor()
+    {
+        //Gathers the name of the selected folder
+        selectedObjectName = Selection.activeObject.name;
+
+        if (AssetDatabase.IsValidFolder("Assets/Materials"))
+        {
+            //Create a new material with the folder's name
+            passMaterial = new Material(Shader.Find("Standard"));
+            AssetDatabase.CreateAsset(passMaterial, "Assets/Materials/" + selectedObjectName + ".mat");
+        }
+        else
+        {
+            AssetDatabase.CreateFolder("Assets", "Materials");
+            //Create a new material with the folder's name
+            passMaterial = new Material(Shader.Find("Standard"));
+            AssetDatabase.CreateAsset(passMaterial, "Assets/Materials/" + selectedObjectName + ".mat");
+        }
+        
+        if (passMaterial == null) return;
+
+        string folderPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+        passMaterial.SetTexture("_MainTex", (Texture2D)AssetDatabase.LoadAssetAtPath(folderPath + "/" + selectedObjectName + File.ReadLines("ColinToolsPreferences.txt").ElementAt(0) + ".png", typeof(Texture2D)));
+        passMaterial.SetTexture("_MetallicGlossMap", (Texture2D)AssetDatabase.LoadAssetAtPath(folderPath + "/" + selectedObjectName + "_Metallic.png", typeof(Texture2D)));
+        passMaterial.SetTexture("_ParallaxMap", (Texture2D)AssetDatabase.LoadAssetAtPath(folderPath + "/" + selectedObjectName + "_Height.png", typeof(Texture2D)));
+        passMaterial.SetTexture("_BumpMap", (Texture2D)AssetDatabase.LoadAssetAtPath(folderPath + "/" + selectedObjectName + "_Normal.png", typeof(Texture2D)));
+        passMaterial.SetTexture("_OcclusionMap", (Texture2D)AssetDatabase.LoadAssetAtPath(folderPath + "/" + selectedObjectName + "_Ambient_occlusion.png", typeof(Texture2D)));
+        //Need a type name for default\/
+        passMaterial.SetTexture("_DetailAlbedoMap", (Texture2D)AssetDatabase.LoadAssetAtPath(folderPath + "/" + selectedObjectName + "_DetailAlbedoMap.png", typeof(Texture2D)));
+        passMaterial.SetTexture("_DetailNormalMap", (Texture2D)AssetDatabase.LoadAssetAtPath(folderPath + "/" + selectedObjectName + "_DetailNormalMap.png", typeof(Texture2D)));
+    }
+}
