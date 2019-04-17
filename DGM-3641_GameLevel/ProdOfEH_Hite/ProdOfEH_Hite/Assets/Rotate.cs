b0f55da4562a9600a97cbb216a517c06fdc2ba0b
Colin <colin.hite@gmail.com>
2019-04-17 12:33:54 -0600

    Got started on the other textures

---
+++ DGM-3641_GameLevel/ProdOfEH_Hite/ProdOfEH_Hite/Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public GameObject rotateObject;
+
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateObject.transform.Rotate(0,speed * Time.deltaTime,0);
+    }
+}
